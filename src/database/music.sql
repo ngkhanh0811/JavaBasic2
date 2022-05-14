8058c2b8ec94e067d7812e074bad65029527a4a9
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-05-14 14:32:28 +0700

    up

--- src/database/music.sql
+++ src/database/music.sql
@@ -1,3 +1,4 @@
+use bookstore;
 create table Music(
                     id int,
                     name varchar(50),
@@ -5,3 +6,6 @@ create table Music(
                     year int,
                     PRIMARY KEY (id)
 );
+insert into music values (7,'All I Ask','Adele',2021),
+                         (8,'Shape of you','Edsheeran',2022),
+                         (9,'Happier','Edsheeran',2021)
